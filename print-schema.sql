fc4f6c0a12a8be1ee47678d40b111c2a0f88fd85
James Reid <jreidmke@gmail.com>
2021-02-05 16:29:03 -0600

    completed schema

--- print-schema.sql
+++ print-schema.sql
@@ -1,18 +1,26 @@
+--MAIN TABLES
+
 CREATE TABLE writers (
     username VARCHAR(20) PRIMARY KEY,
     password TEXT NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
+    image_url TEXT,
     bio TEXT NOT NULL,
     age INTEGER NOT NULL,
     location TEXT NOT NULL,
-    email TEXT NOT NULL,
+    email TEXT NOT NULL CHECK (position('@' IN email) > 1),
     phone CHAR(10),
-    twitter_username TEXT NOT NULL,
-    facebook_username TEXT NOT NULL,
-    youtube_username TEXT NOT NULL,
-    is_admin BOOLEAN
-); 
+    twitter_username TEXT,
+    facebook_username TEXT,
+    youtube_username TEXT,
+    is_admin BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE tags (
+    tag TEXT PRIMARY KEY,
+    is_fiction BOOLEAN NOT NULL
+);
 
 CREATE TABLE portfolios (
     id SERIAL PRIMARY KEY,
@@ -29,4 +37,102 @@ CREATE TABLE pieces (
     text TEXT NOT NULL,
     date_of_submission TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     date_of_last_edit TIMESTAMP DEFAULT NULL
+);
+
+CREATE TABLE platforms (
+    name VARCHAR(25) PRIMARY KEY,
+    password TEXT NOT NULL,
+    location TEXT NOT NULL,
+    description TEXT NOT NULL,
+    logo_url TEXT,
+    email TEXT UNIQUE NOT NULL CHECK (position('@' IN email) > 1),
+    phone CHAR(10) NOT NULL,
+    twitter_username TEXT,
+    facebook_username TEXT,
+    youtube_username TEXT,
+    is_admin BOOLEAN DEFAULT FALSE
+); 
+
+CREATE TABLE gigs (
+    id SERIAL PRIMARY KEY,
+    platform_name VARCHAR(25) NOT NULL
+        REFERENCES platforms ON DELETE CASCADE,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    compensation INTEGER CHECK (compensation > 0),
+    is_remote BOOLEAN NOT NULL,
+    gig_type TEXT NOT NULL CHECK (gig_type IN ('Fulltime', 'Recurring', 'One-Time')),
+    date_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+--MANY TO MANY TABLES
+
+CREATE TABLE applications (
+    id SERIAL PRIMARY KEY,
+    writer_username TEXT
+        REFERENCES writers ON DELETE CASCADE,
+    platform_name VARCHAR(25)
+        REFERENCES platforms ON DELETE CASCADE,
+    gig_id INTEGER
+        REFERENCES gigs ON DELETE CASCADE,
+    portfolio_id INTEGER
+        REFERENCES portfolios ON DELETE CASCADE,
+    is_active BOOLEAN NOT NULL
+);
+
+CREATE TABLE portfolio_pieces (
+    portfolio_id INTEGER
+        REFERENCES portfolios ON DELETE CASCADE,
+    piece_id INTEGER 
+        REFERENCES pieces ON DELETE CASCADE,
+    PRIMARY KEY (portfolio_id, piece_id)
+);
+
+CREATE TABLE piece_tags (
+    tag_title TEXT
+        REFERENCES tags ON DELETE CASCADE,
+    piece_id TEXT
+        REFERENCES tags ON DELETE CASCADE,
+    PRIMARY KEY (tag_title, piece_id)
+); 
+
+CREATE TABLE gig_tags (
+    gig_id INTEGER
+        REFERENCES gigs ON DELETE CASCADE,
+    tag_title TEXT
+        REFERENCES tags ON DELETE CASCADE,
+    PRIMARY KEY (gig_id, tag_title)
+); 
+
+CREATE TABLE writer_follows_tag (
+    writer_username TEXT
+        REFERENCES writers ON DELETE CASCADE,
+    tag_title TEXT
+        REFERENCES tags ON DELETE CASCADE,
+    PRIMARY KEY (writer_username, tag_title)
+); 
+
+CREATE TABLE writer_follows_platform (
+    writer_username VARCHAR(20)
+        REFERENCES writers ON DELETE CASCADE,
+    platform_name VARCHAR(25)
+        REFERENCES platforms ON DELETE CASCADE,
+    PRIMARY KEY (writer_username, platform_name)
+);
+
+CREATE TABLE platform_follows_tag (
+    platform_name VARCHAR(25)
+        REFERENCES platforms ON DELETE CASCADE,
+    tag_title TEXT
+        REFERENCES tags ON DELETE CASCADE,
+    PRIMARY KEY (platform_name, tag_title)
+);
+
+CREATE TABLE platform_follows_writer (
+    platform_name VARCHAR(25)
+        REFERENCES platforms ON DELETE CASCADE,
+    writer_username VARCHAR(20)
+        REFERENCES writers ON DELETE CASCADE,
+    PRIMARY KEY (writer_username, platform_name)
 );
--- print-seed.sql
+++ print-seed.sql
@@ -27,3 +27,50 @@ VALUES ('test1',
     'testadmin',
     'testadmin',
     TRUE);
+
+INSERT INTO portfolios (writer_username, title)
+VALUES ('test1', 'Test Portfolio 1'), ('test2', 'Test Portfolio 2'); 
+
+INSERT INTO pieces (writer_username, title, text)
+VALUES ('test1', 
+    'Test Piece 1', 
+    'Remember, a Jedi can feel the Force flowing through him. I don''t know what you''re talking about. I am a member of the Imperial Senate on a diplomatic mission to Alderaan-- Alderaan? I''m not going to Alderaan. I''ve got to go home. It''s late, I''m in for it as it is.
+    You mean it controls your actions? Your eyes can deceive you. Don''t trust them. But with the blast shield down, I can''t even see! How am I supposed to fight? The more you tighten your grip, Tarkin, the more star systems will slip through your fingers.
+    Look, I can take you as far as Anchorhead. You can get a transport there to Mos Eisley or wherever you''re going.'),
+    ('test2',
+    'Test Piece 2', 
+    'Remember, a Jedi can feel the Force flowing through him. I don''t know what you''re talking about. I am a member of the Imperial Senate on a diplomatic mission to Alderaan-- Alderaan? I''m not going to Alderaan. I''ve got to go home. It''s late, I''m in for it as it is.
+    You mean it controls your actions? Your eyes can deceive you. Don''t trust them. But with the blast shield down, I can''t even see! How am I supposed to fight? The more you tighten your grip, Tarkin, the more star systems will slip through your fingers.
+    Look, I can take you as far as Anchorhead. You can get a transport there to Mos Eisley or wherever you''re going.'
+    );
+
+--both platforms have the password "password"
+
+INSERT INTO platforms(name, password, location, description, logo_url, email, phone, twitter_username, facebook_username, youtube_username, is_admin)
+VALUES ('platform1',
+    '$2b$12$AZH7virni5jlTTiGgEg4zu3lSvAw68qVEfSIOjJ3RqtbJbdW/Oi5q',
+    'Milwaukee',
+    'Hey, Luke! May the Force be with you. In my experience, there is no such thing as luck. The more you tighten your grip, Tarkin, the more star systems will slip through your fingers. Kid, I''ve flown from one side of this galaxy to the other. I''ve seen a lot of strange stuff, but I''ve never seen anything to make me believe there''s one all-powerful Force controlling everything. There''s no mystical energy field that controls my destiny. It''s all a lot of simple tricks and nonsense.',
+    'https://miro.medium.com/max/8000/1*JrHDbEdqGsVfnBYtxOitcw.jpeg',
+    'platform1@platform1.com',
+    '1111111111',
+    'platform1',
+    'platform1',
+    'platform1',
+    FALSE), 
+    ('platform2',
+    '$2b$12$AZH7virni5jlTTiGgEg4zu3lSvAw68qVEfSIOjJ3RqtbJbdW/Oi5q',
+    'Chicago',
+    'Hey, Luke! May the Force be with you. In my experience, there is no such thing as luck. The more you tighten your grip, Tarkin, the more star systems will slip through your fingers. Kid, I''ve flown from one side of this galaxy to the other. I''ve seen a lot of strange stuff, but I''ve never seen anything to make me believe there''s one all-powerful Force controlling everything. There''s no mystical energy field that controls my destiny. It''s all a lot of simple tricks and nonsense.',
+    'https://miro.medium.com/max/8000/1*JrHDbEdqGsVfnBYtxOitcw.jpeg',
+    'platform2@platform2.com',
+    '2222222222',
+    'platform2',
+    'platform2',
+    'platform2',
+    TRUE);
+
+INSERT INTO gigs (platform_name, title, description, compensation, is_remote, gig_type)
+VALUES ('platform1', 
+    'Comedy Writing Gig', 'Write us a funny    script for our youtube sketch channel', 75, TRUE, 'One-Time'),
+    ('platform2', 'New Article about Tammy Baldwin', 'We need someone to write us a story about Tammy Baldwin. If this goes well, there is potential for recurring work.', 100, FALSE, 'Recurring'); 
