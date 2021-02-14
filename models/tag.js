c7c377cd118037f6897a00b3e51090d4baf39e3f
James Reid <jreidmke@gmail.com>
2021-02-14 11:04:48 -0600

    completed tag doc strings and refactor

--- models/tag.js
+++ models/tag.js
@@ -2,21 +2,42 @@
 const db = require("../db");
 
 class Tag {
+
+   /** Find all tags (optional filter on searchFilters).
+     *
+     * searchFilters (all optional):
+     * - isFiction
+     * - search
+     *
+     * Returns [{ id, title, isFiction, created_at, updated_at }, ...]
+     * */
     
-    static async getAll() {
-      const results = await db.query(`SELECT * FROM tags`);
+    static async getAll(searchFilters = {}) {
+      let query = `SELECT * FROM tags`;
+      let whereExpressions = [];
+      let queryValues = [];
+      const { search, isFiction }= searchFilters;
+
+      if(isFiction !== undefined) {
+        queryValues.push(isFiction);
+        whereExpressions.push(`is_fiction=$${queryValues.length}`);
+      };
+
+      if(whereExpressions.length > 0) {
+        query += " WHERE " + whereExpressions.join(" AND ");
+      };
+
+      if(search !== undefined) {
+        if(!whereExpressions.length) {
+          query += ` WHERE title LIKE '%${search}%'`;
+        } else {
+          query += ` AND title LIKE '%${search}%'`;
+        };
+      };
+
+      const results = await db.query(query, queryValues);
       return results.rows;
     };
-
-    static async getByIsFiction(isFiction) {
-      const results = await db.query(`SELECT * FROM tags WHERE is_fiction=$1`, [isFiction]);
-      return results.rows;
-    };
-
-    static async getBySearch(searchTerm) {
-      const results = await db.query(`SELECT * FROM tags WHERE title LIKE '%${searchTerm}%'`);
-      return results.rows;
-    }
 };
 
 module.exports = Tag;
--- routes/tags.js
+++ routes/tags.js
@@ -1,38 +1,16 @@
 const express = require("express");
-const { BadRequestError } = require("../expressError");
 const { ensureLoggedIn } = require("../middleware/auth");
 const router = new express.Router();
 const Tag = require("./../models/tag");
 
-router.get("/", ensureLoggedIn, async function(req, res, next) {
+router.get("/", ensureLoggedIn, async(req, res, next) => {
     const q = req.query;
-    let tags;
-    if(q.fiction || q.search) {
-        try {
-            if(q.fiction) {
-                if(q.fiction === "true") {
-                    tags = await Tag.getByIsFiction(true);
-                } else if(q.fiction === "false") {
-                    tags = await Tag.getByIsFiction(false);
-                } else {
-                    throw new BadRequestError("Fiction query must equal 'true' or 'false'.");
-                };
-            } else if(q.search) {
-                tags = await Tag.getBySearch(q.search);
-            } else {
-                throw new BadRequestError("Query Must be either 'search' or fiction'");
-            }
-            return res.json({ tags });
-        } catch (error) {
-            return next(error);
-        }
-    }
     try {
-        tags = await Tag.getAll();
+        const tags = await Tag.getAll(q);
         return res.json({ tags });
     } catch (error) {
         return next(error);
     }
-});
+})
 
 module.exports = router;
