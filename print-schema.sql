0be9ad0b61ee087563623d314747cbdb0931594d
James Reid <jreidmke@gmail.com>
2021-03-14 11:41:54 -0500

    deploy

--- print-schema.sql
+++ print-schema.sql
@@ -165,6 +165,15 @@ CREATE TABLE queries(
     UNIQUE (writer_id, platform_id, gig_id)
 );
 
+CREATE TABLE ongoing_gigs(
+    id SERIAL PRIMARY KEY,
+    gig_id INTEGER REFERENCES gigs(id) ON DELETE CASCADE,
+    writer_id INTEGER REFERENCES writers(id) ON DELETE CASCADE,
+    platform_id INTEGER REFERENCES platforms(id) ON DELETE CASCADE,
+    deadline DATE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE application_messages(
     id SERIAL PRIMARY KEY,
     application_id INTEGER REFERENCES applications(id) ON DELETE CASCADE,
@@ -173,14 +182,5 @@ CREATE TABLE application_messages(
     portfolio_id INTEGER REFERENCES portfolios(id) ON DELETE CASCADE,
     gig_id INTEGER REFERENCES gigs(id) ON DELETE CASCADE,
     status VARCHAR NOT NULL CHECK (status in('Pending', 'Accepted', 'Rejected')) DEFAULT 'Pending',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE ongoing_gigs(
-    id SERIAL PRIMARY KEY,
-    gig_id INTEGER REFERENCES gigs(id) ON DELETE CASCADE,
-    writer_id INTEGER REFERENCES writers(id) ON DELETE CASCADE,
-    platform_id INTEGER REFERENCES platforms(id) ON DELETE CASCADE,
-    deadline DATE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
